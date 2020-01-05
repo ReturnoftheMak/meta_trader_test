f19e6d8022d765359f6a8edd3e1d231c9ba3b68d
ReturnoftheMak <50878639+ReturnoftheMak@users.noreply.github.com>
2020-01-05 21:44:13 +0000

    higher timeframe logic

--- meta_trader_test/h4_strategy.py
+++ meta_trader_test/h4_strategy.py
@@ -1,6 +1,7 @@
 #%% Package imports
 
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 from MetaTrader5 import MT5CopyRatesFromPos, MT5_TIMEFRAME_D1
 from pytz import timezone
 from rate_transformation import convert_rate_tuple
@@ -21,6 +22,38 @@ def high_timeframe_check(currency):
     x = MT5CopyRatesFromPos(currency, MT5_TIMEFRAME_D1, 0, 200)
 
     df = convert_rate_tuple(x)
+
+    latest_date = df.index.max()
+
+    # This gives the latest date, from which we will find the timestamp
+    # for 3 and 6 months prior, and use this to return the indices for
+    # the required rows. It should be noted that weekday only trading
+    # will mean we need to get to the weekday preceding the 3/6 month
+
+    def get_preceding_weekday(latest_date, month_delta, df):
+        """Get the date from
+        """
+
+        check_date = latest_date + relativedelta(months=-month_delta)
+
+        while len(df[str(check_date).split(' ')[0]]) < 1:
+            check_date += relativedelta(days=-1)
+
+        return check_date
+
+    check_3_month = get_preceding_weekday(latest_date, 3, df)
+    check_6_month = get_preceding_weekday(latest_date, 6, df)
+
+    # Get the row using the timestamp with the index
+    df_3month = df[str(check_3_month).split(' ')[0]]
+    df_6month = df[str(check_6_month).split(' ')[0]]
+    df_current = df[str(latest_date).split(' ')[0]]
+
+    # Check the logic for this pair
+
+
+
+#%%
 
 usdchf_rates = high_timeframe_check("USDCHF")
 
