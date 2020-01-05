47069e22791235884799316aa157ed0a4fa2b649
ReturnoftheMak <50878639+ReturnoftheMak@users.noreply.github.com>
2020-01-05 16:37:45 +0000

    dataframe creation function

---
+++ meta_trader_test/rate_transformation.py
@@ -0,0 +1,32 @@
+#%% Package imports
+
+from datetime import datetime
+from MetaTrader5 import *
+import pandas as pd
+from pytz import timezone
+import matplotlib.pyplot as plt
+utc_tz = timezone('UTC')
+
+
+#%% Convert rates tuple to DataFrame
+
+def convert_rate_tuple(rates):
+    """Converts the tuple received from server into a dataframe
+    """
+
+    data = {'time':[x.time.astimezone(utc_tz) for x in rates],
+            'open':[x.open for x in rates],
+            'close':[x.close for x in rates],
+            'high':[x.high for x in rates],
+            'low':[x.low for x in rates],
+            'tick_volume':[x.tick_volume for x in rates],
+            'spread':[x.spread for x in rates],
+            'real_volume':[x.real_volume for x in rates]
+            }
+
+    df = pd.DataFrame(data)
+
+    return df
+
+
+#%%
