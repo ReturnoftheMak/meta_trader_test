91db72fd44db51170dbe40b75a43b0e0287730c1
ReturnoftheMak <50878639+ReturnoftheMak@users.noreply.github.com>
2020-02-15 17:57:36 +0000

    basic h4 functions

--- meta_trader_test/h4_functions.py
+++ meta_trader_test/h4_functions.py
@@ -17,7 +17,7 @@ connect()
 currencies = ['USDCHF', 'EURUSD', 'GBPUSD', 'USDJPY']
 
 
-# %% testers
+# %% Conversion function
 
 def rates_to_dataframe(currency='', timeframe=0, periods=0):
     """Convert the ndarray received from copy_rates to a dataframe
@@ -32,9 +32,9 @@ def rates_to_dataframe(currency='', timeframe=0, periods=0):
     """
 
     # Use the copy_rates
-    x = mt5.copy_rates_from_pos(currency, timeframe, 0, periods)
+    rates = mt5.copy_rates_from_pos(currency, timeframe, 0, periods)
 
-    df = pd.DataFrame(x)
+    df = pd.DataFrame(rates)
 
     # Localize to UTC from ns-epoch
     df.time = df.time.apply(datetime.fromtimestamp).dt.tz_localize(utc_tz)
@@ -45,7 +45,75 @@ def rates_to_dataframe(currency='', timeframe=0, periods=0):
     return df
 
 
-# %%
+# %% Get df
 
 df = rates_to_dataframe('EURUSD', mt5.TIMEFRAME_D1, 2000)
+
+
+
+# %% What we actually need is to give a date and return info about that day
+# For a given day, a function for the 3/6 month prior date or closest
+# Get the D1 timeframe data for that given day
+
+# We do need to check if there is any data for the date we check,
+# but that doesn't need to be done by the first function
+# Can implement a relativedelta function with recursion to get the date we want
+
+# Thankfully selecting a weekend date simply returns the latest D1 data, 
+# So we'll keep this behaviour
+
+def get_preceding_weekday(date, n):
+    """Get the date for the relevant day n months prior
+
+    Params:
+        date (pandas._libs.tslibs.timestamps.Timestamp): date in question
+        n (int): number of months
+    
+    Returns:
+        date to pass (pandas._libs.tslibs.timestamps.Timestamp)
+    """
+
+    check_date = date + relativedelta(months=-n)
+
+    return check_date
+
+
+def daily_timeframe_data(currency, date):
+    """Returns a single days data
+    """
+
+    rates = mt5.copy_rates_from(currency, mt5.TIMEFRAME_D1, date, 1)
+
+    df = pd.DataFrame(rates)
+    df.time = df.time.apply(datetime.fromtimestamp).dt.tz_localize(utc_tz)
+
+    df.set_index('time', inplace=True)
+
+    return df
+
+
+# %%
+
+rate_dict_3 = daily_timeframe_data('EURUSD', get_preceding_weekday(df.index[-1], 3))
+rate_dict_6 = daily_timeframe_data('EURUSD', get_preceding_weekday(df.index[-1], 6))
+
+
+# %% Write func to get tick data for a given day
+
+def get_ticks_for_day(currency, date):
+    """Get all the ticks for a given day
+    """
+
+    to_date = date + relativedelta(days=1)
+
+    ticks = mt5.copy_ticks_range(currency, date, to_date, mt5.COPY_TICKS_ALL)
+
+    df = pd.DataFrame(ticks)
+    df.time = df.time.apply(datetime.fromtimestamp).dt.tz_localize(utc_tz)
+
+    df.set_index('time', inplace=True)
+
+    return df
+
+
 
